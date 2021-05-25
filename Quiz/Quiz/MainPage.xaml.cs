b14b3f742fd6c5bbadf61ce0dcfe7d92a4928cf7
Christian Meneghetti <dtisistemas@araras.sp.gov.br>
2021-05-25 11:11:59 -0300

    Criação da pagina 2 do quiz.

--- Quiz/Quiz/MainPage.xaml.cs
+++ Quiz/Quiz/MainPage.xaml.cs
@@ -14,5 +14,10 @@ namespace Quiz
         {
             InitializeComponent();
         }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Page1());
+        }
     }
 }
---
+++ Quiz/Quiz/Page1.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Quiz
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page1 : ContentPage
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            String texto = eResposta.Text;
+            if(texto.ToUpper() == "HUNTER")
+            {
+                Navigation.PushAsync(new Page2());
+            }
+            else
+            {
+
+            }
+        }
+    }
+}
